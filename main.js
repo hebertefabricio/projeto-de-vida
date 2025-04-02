6dbfc1191e6b97e8e2b0c99698eb9444f7f525fa
hebertefabricio <148267252+hebertefabricio@users.noreply.github.com>
2025-04-02 14:18:10 +0000

    não terminei

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.lenght;i++){
     botoes[i].onclick = function(){
 
-        for(let j=0;j<botoes.length;++){
-            botoes[].classList.remove("ativo");
+        for(let j=0;j<botoes.length;i++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-    console.log();
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-04-02T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1){
+    let tempoAtua1 = new Date();
+    let tempoFina1 = tempoObjetivo - tempoAtua1;
+    let segundos = Math.floor(tempoFina1/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    return minutos;
+}
+
+
